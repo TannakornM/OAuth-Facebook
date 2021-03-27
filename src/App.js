97142fe3d6f4f20d6af3e301162a0e838bf6f04e
realchanin <6110110589@psu.ac.th>
2021-03-28 00:36:35 +0700

    employee list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 // import Map from './Map'
 import Facebook from './components/Facebook'
+
 function App() {
   return (
     <div className="App">
--- src/components/Facebook.js
+++ src/components/Facebook.js
@@ -1,24 +1,53 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 import FacebookLogin from 'react-facebook-login';
+import { Card, Image } from 'react-bootstrap';
+import '../App.css';
 
-export default class Facebook extends Component {
-    componentClicked = () => {
-        console.log('click')
+function App() {
+  
+  const [login, setLogin] = useState(false);
+  const [data, setData] = useState({});
+  const [picture, setPicture] = useState('');
+
+  const responseFacebook = (response) => {
+    console.log(response);
+    setData(response);
+    setPicture(response.picture.data.url);
+    if (response.accessToken) {
+      setLogin(true);
+    } else {
+      setLogin(false);
     }
-    responseFacebook = (response) => {
-        console.log(response);
-    }
-    render() {
-        return (
-            <div>
-                <FacebookLogin
-                    appId="637001937118804"
-                    autoLoad={true}
-                    fields="name,email,picture"
-                    onClick={this.componentClicked}
-                    callback={this.responseFacebook} />
-            </div>)
-    }
+  }
+
+  return (
+    <div class="container">
+      <Card style={{ width: '300px' }}>
+        <Card.Header>
+          { !login && 
+            <FacebookLogin
+              appId="637001937118804"
+              autoLoad={true}
+              fields="name,email,picture"
+              scope="public_profile,user_friends"
+              callback={responseFacebook}
+              icon="fa-facebook" />
+          }
+          { login &&
+            <Image src={picture} roundedCircle />
+          }
+        </Card.Header>
+        { login &&
+          <Card.Body>
+            <Card.Title>{data.name}</Card.Title>
+            <Card.Text>
+              {data.email}
+            </Card.Text>
+          </Card.Body>
+        }
+      </Card>
+    </div>
+  );
 }
-    
+
+export default App;
